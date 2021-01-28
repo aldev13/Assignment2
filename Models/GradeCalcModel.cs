81f29eea3c28a4b3bbe89bc58b0f604fc40b5b16
aldev13 <aldev13@gmail.com>
2021-01-27 20:09:41 -0700

    Add project files.

---
+++ Models/GradeCalcModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class GradeCalcModel
+    {
+        [Range(0, 50, ErrorMessage = "Assignment % must be between 0 and 50")]
+        public int Assign { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Project % must be between 0 and 10")]
+        public int Project { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Quiz % must be between 0 and 10")]
+        public int Quiz { get; set; }
+
+        [Range(0, 20, ErrorMessage = "Exam % must be between 0 and 20")]
+        public int Exam { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Intex % must be between 0 and 10")]
+        public int Intex { get; set; }
+    }
+}
---
+++ wwwroot/js/gradecalc.js
@@ -0,0 +1,90 @@
+﻿//Program calculates the percentage entered in by the user
+//Which then gives an alert to the total percentage and letter grade earned
+
+
+$("#submitButton").click( function () {
+
+    //Create Variables
+    var assign, project, quiz, exam, intex, total, grade;
+
+    //Assign Variables
+    assign = parseInt($("#Assign").val());
+    project = parseInt($("#Project").val());
+    quiz = parseInt($("#Quiz").val());
+    exam = parseInt($("#Exam").val());
+    intex = parseInt($("#Intex").val());
+
+    //Calculate total percentage
+    total = assign + project + quiz + exam + intex
+
+    //Determine letter grade and alert the result
+    if (total >= 101)
+    {
+        alert("This is not a real grade");
+    }
+    else if (total >= 94)
+    {
+        grade = "A";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if (total >= 90)
+    {
+        grade = "A-";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 87)
+    {
+        grade = "B+";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 84)
+    {
+        grade = "B";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 94)
+    {
+        grade = "A";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 80)
+    {
+        grade = "B-";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 77)
+    {
+        grade = "C+";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 74)
+    {
+        grade = "C";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 70)
+    {
+        grade = "C-";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 67)
+    {
+        grade = "D+"
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 64)
+    {
+        grade = "D";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else if(total >= 60)
+    {
+        grade = "D-";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade);
+    }
+    else
+    {
+        grade = "E";
+        alert("Your total percentage is " + total + "%! And your letter grade is " + grade + ", sorry...");
+    }
+});
